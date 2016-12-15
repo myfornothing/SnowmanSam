f4b55e40ec990bf94ac1b5e659e8a78703b80c3b
myfornothing <dadiiforno@gmail.com>
2016-12-14 21:49:52 -0500

    Add files via upload

---
+++ Spike.java
@@ -0,0 +1,74 @@
+package com.fornothing.snowmansam.entities;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.fornothing.snowmansam.MainClass;
+import com.fornothing.snowmansam.utilities.CollisionRect;
+
+public class Spike {
+
+    public static int getInitialSpeed() {
+        return INITIAL_SPEED;
+    }
+
+    private static final int INITIAL_SPEED = 20;
+    private static final int ACCELERATION = 175;
+    private static final int GOAL_SPEED = 400;
+    public static final int WIDTH = 24;
+    public static final int HEIGHT = 60;
+    public static int spikeCount = 0;
+    private static Texture spikeTexture;
+    private float x, y;
+    private int speed;
+    private int goalspeed;
+
+    private CollisionRect rect;
+    public boolean remove = false;
+
+    public Spike(float x) {
+        this.x = x;
+        y = MainClass.V_HEIGHT;
+        speed = INITIAL_SPEED;
+        goalspeed = GOAL_SPEED;
+        this.rect = new CollisionRect(x, y, WIDTH, HEIGHT);
+
+        if (spikeTexture == null) {
+            spikeTexture = new Texture("images/Ice_Spike_1.png");
+        }
+        this.rect = new CollisionRect(
+                x + spikeTexture.getWidth() /2,
+                y + spikeTexture.getHeight() * 0.2f,
+                WIDTH - spikeTexture.getWidth() /2,
+                HEIGHT - spikeTexture.getHeight() /2);
+    }
+
+    public void update (float deltaTime) {
+        //Speed adjustment to reach goal
+        if (speed < goalspeed) {
+            speed += ACCELERATION * deltaTime;
+            if (speed > goalspeed)
+                speed = goalspeed;
+        }
+        speed += ACCELERATION * deltaTime;
+        y -= speed * deltaTime;
+        if (y < -MainClass.V_HEIGHT) {
+            remove = true;
+        }
+        rect.move(x, y);
+    }
+
+    public void render (SpriteBatch batch) {
+        batch.draw(spikeTexture, x, y, WIDTH, HEIGHT);
+    }
+
+    public CollisionRect getCollisionRect () {
+        return rect;
+    }
+    public static int getSpikeCount() { return spikeCount; }
+    public float getX () {
+        return x;
+    }
+    public float getY() {
+        return y;
+    }
+}
